7a7daf6af0eea01361c1515d7662ffa2f579c59e
keebmill <keelahmiller@Keelahs-MacBook-Air.local>
2022-06-25 16:05:33 -0400

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-// Assignment code here
-
-
+/// Assignment code here
+window.alert ("Click button for password!!!");
+ 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = document.getElementById("password");
   var passwordText = document.querySelector("#password");
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var passwordLength = 9;
+  var password = "";
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+  }
 
-  passwordText.value = password;
-
+  document.getElementById("password").value = password;
+ 
 }
-
+ 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
