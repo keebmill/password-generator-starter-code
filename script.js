7e3974afd6e597220cf1c9a492e28cb1c82af715
keebmill <keelahmiller@Keelahs-MacBook-Air.local>
2022-06-25 23:51:38 -0400

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,62 @@
 /// Assignment code here
 window.alert ("Click button for password!!!");
- 
+
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numberstEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const generateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
+
+
+ const randomFunc = {
+   lower: getRandonLower,
+   upper: getRandomUpper,
+   number: getRandomNumber,
+   Symbol: getRandomSymbol
+ };
+
+generateEl.addEventListener('click' , () =>{
+  const length = lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbols = symbolsEl.checked;
+
+  resuleEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbols,
+    length
+  );
+
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+  
+}
+
+// Generator functions
+function getRandonLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26)+ 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26)+ 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10)+ 48);
+}
+
+function getRandomSymbol() {
+  const Symbols = '!@#$%^&*()=<>/+-.'
+  return Symbols = [Math.floor(Math.random() * Symbol.length)];
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
